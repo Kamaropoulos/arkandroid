365b6509cc14d5934f306799dbd6f5c393a07c5e
Kamaropoulos <kamaropoulos@outlook.com>
2018-06-06 16:50:17 +0300

    Added PaddleController source code

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PaddleController : MonoBehaviour {
+
+    [Range(1, 20)]
+    public int speed = 5;
+    public float moveStep = 0.5f;
+    private Rigidbody2D rigidbody = null;
 
 	// Use this for initialization
 	void Start () {
-		
+        rigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float moveXY = Input.GetAxis("Horizontal");
+        float x = moveXY * speed * moveStep;
+        rigidbody.velocity = new Vector2(x, 0);
 	}
 }
