9475edd9de2ce14cecee43eb612f277eeecdd74a
Kamaropoulos <kamaropoulos@outlook.com>
2018-06-07 21:42:21 +0300

    Added Block Prefab Changes on the BlockController script

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour {
 
+    //public GameObject block;
+
     private float force = 200f;
     private Rigidbody2D rigidbody = null;
     private bool started = false;
@@ -11,6 +13,9 @@ public class BallController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody2D>();
+
+        //GameObject newBlock = Instantiate(block, transform.position, transform.rotation);
+        //newBlock.GetComponent<BlockController>().Initialize("yellow", 5);
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -1,20 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class BlockController : MonoBehaviour
-{
+public class BlockController : MonoBehaviour {
+
+    public Sprite[] sprites;
+    public Dictionary<string, Sprite> spritesDictionary = new Dictionary<string, Sprite>();
 
     public int lives = 1;
 
     // Use this for initialization
     void Start() {
-
-    }
-
-    // Update is called once per frame
-    void Update() {
-
+        UpdateSpritesDictionary();
     }
 
     void OnTriggerExit2D(Collider2D col) {
@@ -23,4 +22,23 @@ public class BlockController : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+
+    public void Initialize(string color, int lives) {
+        UpdateSpritesDictionary();
+        SetColor(color);
+        this.lives = lives;
+    }
+
+    void SetColor(string color) {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = spritesDictionary[color];
+    }
+
+    void UpdateSpritesDictionary() {
+        foreach (Sprite sprite in sprites) {
+            if (!spritesDictionary.ContainsKey(sprite.name)) {
+                spritesDictionary.Add(sprite.name, sprite);
+            }
+        }
+    }
+
 }
