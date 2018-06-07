8e2116f3eab00d3f6e136ea6f779c72b2fcba403
Kamaropoulos <kamaropoulos@outlook.com>
2018-06-07 15:04:20 +0300

    Added block and basic block destruction

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+
+    public int lives = 1;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        lives--;
+        if (lives < 1) {
+            Destroy(this.gameObject);
+        }
+    }
+}
