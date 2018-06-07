07776684d9da722aa80db0880b147df58c502852
Kamaropoulos <kamaropoulos@outlook.com>
2018-06-07 10:45:52 +0300

    Ball Physics and Logic

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private float force = 200f;
+    private Rigidbody2D rigidbody = null;
+    private bool started = false;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!started && Input.GetKeyUp(KeyCode.Space)) {
+            transform.SetParent(null);
+            rigidbody.bodyType = RigidbodyType2D.Dynamic;
+            rigidbody.gravityScale = 0;
+            rigidbody.AddForce(new Vector2(force, force));
+            started = true;
+        }
+	}
+}
